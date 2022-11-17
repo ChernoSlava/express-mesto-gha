bd9f27a87163c18883efbf2d1127e6411d776bba
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2022-11-17 22:30:34 +0300

    fix: proba

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
+      // name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().required().pattern(/^(https?:\/\/)?([\w-]+\.[\w-]+)\S*$/, 'URL'),
     }),
